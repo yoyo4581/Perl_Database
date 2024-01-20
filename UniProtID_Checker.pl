ae9b9206b3d072e29c4fe93adb75b9b483b12e8d
Yahya Al Sabeh <yahyaalsabeh@gmail.com>
2024-01-19 16:22:36 -0800

    Insert1

---
+++ UniProtID_Checker.pl
@@ -0,0 +1,26 @@
+use strict;
+use warnings;
+use autodie;
+
+my $filename = "./test_id.txt";
+open(my $fh, '<:encoding(UTF-8)', $filename) or die "Could not open '$filename' $!";
+open(my $oh, '>:encoding(UTF-8)', "./HW_1_output.txt");
+
+sub Check{
+
+  my $string =  $_[0];
+  if ($string =~ /^[a-zA-Z]{2}\d{4,5}/){
+     print {$oh} $string."\n";
+  }
+  else{
+     print("Error invalid string ".$string."\n");
+  }
+}
+
+
+while (my $row = <$fh>) {
+  $row =~ s/^\s+|\s+$//g;
+  Check($row);
+}  
+close $fh;
+close $oh;
